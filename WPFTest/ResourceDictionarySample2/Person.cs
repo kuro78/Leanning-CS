929047e26a18d79b9c3942befb8680c2f119218d
kuro <kuro78@gmail.com>
2022-04-16 14:23:30 +0900

    ResourceDictionary 사용하기 (활용편)

---
+++ WPFTest/ResourceDictionarySample2/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace ResourceDictionarySample2;
+
+public class Person
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public bool Sex { get; set; }
+    public string Phone { get; set; }
+}
