1c0253061f24eb0529bf5dfcc76e2fc943049814
kuro <kuro78@gmail.com>
2022-04-17 11:15:38 +0900

    동적으로 반응하는 다크 모드, 라이트 모드 테마 적용

---
+++ WPFTest/ThemesSample/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using ThemesSample.Themes;
+
+namespace ThemesSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // loads a png icon, not an ico. 
+            Uri iconUri = new Uri("./Resources/idektbh.png", UriKind.RelativeOrAbsolute);
+            this.Icon = BitmapFrame.Create(iconUri);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (ThemesController.IsDarkThemeApplied() is bool isDarkTheme && isDarkTheme)
+            {
+                ThemesController.CurrentTheme = ThemeTypes.ColourfulDark;
+            }
+            else
+            {
+                ThemesController.CurrentTheme = ThemeTypes.ColourfulLight;
+            }
+        }
+
+        private void ChangeTheme(object sender, RoutedEventArgs e)
+        {
+            switch (int.Parse(((MenuItem)sender).Uid))
+            {
+                case 0:
+                    ThemesController.CurrentTheme = ThemeTypes.Light;
+                    break;
+                case 1:
+                    ThemesController.CurrentTheme = ThemeTypes.ColourfulLight;
+                    break;
+                case 2:
+                    ThemesController.CurrentTheme = ThemeTypes.Dark;
+                    break;
+                case 3:
+                    ThemesController.CurrentTheme = ThemeTypes.ColourfulDark;
+                    break;
+            }
+
+            e.Handled = true;
+        }
+    }
+}
---
+++ WPFTest/ThemesSample/Themes/ThemesController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Windows;
+
+namespace ThemesSample.Themes;
+
+public enum ThemeTypes
+{
+    Light,
+    ColourfulLight,
+    Dark,
+    ColourfulDark
+}
+
+public static class ThemesController
+{
+    private static ThemeTypes? _currentTheme = null;
+    private static ResourceDictionary _currentThemeDictionary = null;
+
+    public static ThemeTypes? CurrentTheme
+    {
+        get => _currentTheme;
+        set
+        {
+            string themeName;
+            switch(value)
+            {
+                case ThemeTypes.Dark:
+                    themeName = nameof(DarkTheme);
+                    break;
+                case ThemeTypes.Light:
+                    themeName = nameof(LightTheme);
+                    break;
+                case ThemeTypes.ColourfulDark:
+                    themeName = nameof(ColourfulDarkTheme);
+                    break;
+                case ThemeTypes.ColourfulLight:
+                    themeName = nameof(ColourfulLightTheme);
+                    break;
+                default:
+                    return;
+            }
+
+            try
+            {
+                if(_currentThemeDictionary != null)
+                {
+                    Application.Current.Resources.MergedDictionaries.Remove(_currentThemeDictionary);
+                }
+
+                var uri = new Uri($"Themes/{themeName}.xaml", UriKind.Relative);
+                _currentThemeDictionary = new ResourceDictionary() { Source = uri };
+                Application.Current.Resources.MergedDictionaries.Add(_currentThemeDictionary);
+            }
+            catch
+            { }
+        }
+    }
+
+    /// <summary>
+    /// Registry를 통해 Window에서 라이트/다크 모드 사용 여부 확인'
+    /// HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize의 AppsUseLightTheme 값이 0이면 다크 테마 사용중
+    /// htttps://stacoverflow.com/questions/51334674/how-to-detect-windows-10-light-dark-mode-in-wind32-application
+    /// </summary>
+    /// <returns></returns>
+    public static bool? IsDarkThemeApplied()
+    {
+        using var personalizeKey = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Themes\Personalize", false);
+        if(personalizeKey != null && personalizeKey.GetValueKind("AppsUseLightTheme") == RegistryValueKind.DWord)
+        {
+            return (int)personalizeKey.GetValue("AppsUseLightTheme", 1) == 0;
+        }
+
+        return null;
+    }
+}
