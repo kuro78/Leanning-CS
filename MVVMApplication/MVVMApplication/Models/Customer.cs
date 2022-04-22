c623945524fb8983e4eb0ab56e384bcafa11ee9d
kuro <kuro78@gmail.com>
2022-04-22 18:53:38 +0900

    Microsoft.Toolkit.Mvvm을 이용한 간단한 프레임워크 part6 - 유효성 검사 추가

--- MVVMApplication/MVVMApplication/Models/Customer.cs
+++ MVVMApplication/MVVMApplication/Models/Customer.cs
@@ -1,28 +1,45 @@
 ﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace MVVMApplication.Models;
 
 public partial class Customer : ObservableValidator
 {
-    public string CustomerID;
     [ObservableProperty]
+    [Required]
+    [MaxLength(5)]
+    private string _customerID;
+    [ObservableProperty]
+    [Required]
+    [MaxLength(40)]
     private string _companyName;
     [ObservableProperty]
+    [MaxLength(30)]
     private string _contactName;
     [ObservableProperty]
+    [MaxLength(30)]
     private string _contactTitle;
     [ObservableProperty]
+    [MaxLength(60)]
     private string _address;
     [ObservableProperty]
+    [MaxLength(15)]
     private string _city;
     [ObservableProperty]
+    [MaxLength(15)]
     private string _region;
     [ObservableProperty]
+    [MaxLength(10)]
     private string _postalCode;
     [ObservableProperty]
+    [MaxLength(15)]
     private string _country;
     [ObservableProperty]
+    [MaxLength(24)]
+    [Phone]
     private string _phone;
     [ObservableProperty]
+    [MaxLength(24)]
+    [Phone]
     private string _fax;
 }
--- MVVMApplication/MVVMApplication/ViewModels/CustomerViewModel.cs
+++ MVVMApplication/MVVMApplication/ViewModels/CustomerViewModel.cs
@@ -6,6 +6,10 @@ using System.Windows.Input;
 using MVVMApplication.Bases;
 using MVVMApplication.Models;
 using MVVMApplication.Services;
+using System.Linq;
+using System.Windows;
+using System.ComponentModel;
+using System;
 
 namespace MVVMApplication.ViewModels;
 
@@ -15,8 +19,13 @@ public partial class CustomerViewModel : ViewModelBase
 
     [ObservableProperty]
     private IList<Customer> _customers;
+    [ObservableProperty]
+    private Customer _selectedCustomer;
+    [ObservableProperty]
+    private string _errorMessage;
 
     public ICommand BackCommand { get; set; }
+    public IRelayCommand SaveCommand { get; set; }
 
     public CustomerViewModel(IDatabaseService databaseService)
     {
@@ -28,6 +37,62 @@ public partial class CustomerViewModel : ViewModelBase
     {
         Title = "Customer";
         BackCommand = new RelayCommand(OnBack);
+        SaveCommand = new RelayCommand(Save,
+                        () => Customers != null
+                        && Customers.Any(c => string.IsNullOrWhiteSpace(c?.CustomerID)));
+
+        PropertyChanging += CustomerViewModel_PropertyChanging;
+        PropertyChanged += CustomerViewModel_PropertyChanged;
+    }
+
+    private void CustomerViewModel_PropertyChanging(object sender, PropertyChangingEventArgs e)
+    {
+        switch(e.PropertyName)
+        {
+            case nameof(SelectedCustomer):
+                if(SelectedCustomer != null)
+                {
+                    SelectedCustomer.ErrorsChanged -= SelectedCustomer_ErrorsChanged;
+                    ErrorMessage = string.Empty;
+                }
+                break;
+        }
+    }
+
+    private void CustomerViewModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+        switch(e.PropertyName)
+        {
+            case nameof(SelectedCustomer):
+                if(SelectedCustomer != null)
+                {
+                    SelectedCustomer.ErrorsChanged += SelectedCustomer_ErrorsChanged;
+                    SetErrorMessage(SelectedCustomer);
+                }
+                break;
+        }
+    }
+
+    private void SelectedCustomer_ErrorsChanged(object sender, DataErrorsChangedEventArgs e)
+    {
+        SetErrorMessage(sender as Customer);
+    }
+
+    private void SetErrorMessage(Customer customer)
+    {
+        if(customer == null)
+        {
+            return;
+        }
+
+        var errors = customer.GetErrors();
+        ErrorMessage = string.Join("\n", errors.Select(e => e.ErrorMessage));
+    }
+
+    private void Save()
+    {
+        MessageBox.Show("Save");
+        SaveCommand.NotifyCanExecuteChanged();
     }
 
     private void OnBack()
@@ -41,5 +106,17 @@ public partial class CustomerViewModel : ViewModelBase
 
         var datas = await _dbService.GetDatasAsync<Customer>("Select * from [Customers]");
         Customers = datas;
+    }
+
+    /// <summary>
+    /// AddCommand
+    /// </summary>
+    [ICommand]
+    private void Add()
+    {
+        var newCustomer = new Customer();
+        Customers.Insert(0, newCustomer);
+        SelectedCustomer = newCustomer;
+        SaveCommand.NotifyCanExecuteChanged();
     }
 }
